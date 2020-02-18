df59340783574bdd8f16210d36644407c720fb82
Yiming Lin <yiming.l@samsung.com>
2020-02-18 16:30:20 +0000

    rm variable

--- mtcnn/first_stage.py
+++ mtcnn/first_stage.py
@@ -1,5 +1,4 @@
 import torch
-from torch.autograd import Variable
 import math
 from PIL import Image
 import numpy as np
@@ -24,24 +23,26 @@ def run_first_stage(image, net, scale, threshold):
     """
 
     # scale the image and convert it to a float array
-    width, height = image.size
-    sw, sh = math.ceil(width*scale), math.ceil(height*scale)
-    img = image.resize((sw, sh), Image.BILINEAR)
-    img = np.asarray(img, 'float32')
+    with torch.no_grad():
+        width, height = image.size
+        sw, sh = math.ceil(width*scale), math.ceil(height*scale)
+        img = image.resize((sw, sh), Image.BILINEAR)
+        img = np.asarray(img, 'float32')
+        img = torch.tensor(_preprocess(img))
+        if torch.cuda.is_available():
+            img = img.cuda()
+        output = net(img)
+        probs = output[1].cpu().numpy()[0, 1, :, :]
+        offsets = output[0].cpu().numpy()
+        # probs: probability of a face at each sliding window
+        # offsets: transformations to true bounding boxes
 
-    img = Variable(torch.FloatTensor(_preprocess(img)), volatile=True)
-    output = net(img)
-    probs = output[1].data.numpy()[0, 1, :, :]
-    offsets = output[0].data.numpy()
-    # probs: probability of a face at each sliding window
-    # offsets: transformations to true bounding boxes
+        boxes = _generate_bboxes(probs, offsets, scale, threshold)
+        if len(boxes) == 0:
+            return None
 
-    boxes = _generate_bboxes(probs, offsets, scale, threshold)
-    if len(boxes) == 0:
-        return None
-
-    keep = nms(boxes[:, 0:5], overlap_threshold=0.5)
-    return boxes[keep]
+        keep = nms(boxes[:, 0:5], overlap_threshold=0.5)
+        return boxes[keep]
 
 
 def _generate_bboxes(probs, offsets, scale, threshold):
